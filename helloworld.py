233ac65479e05fe1b820124f644b0d6916b1f14e
Thomas <pavan_thomas@yahoo.com>
2023-12-28 18:07:31 -0800

    fixed issue

--- helloworld.py
+++ helloworld.py
@@ -2,9 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home():
     return 'Orcodio!!'
 
 @app.route('/admin')
-def hello_world():
+def admin():
     return 'Admin page'
